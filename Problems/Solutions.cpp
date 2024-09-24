4ec112be21bb2deceb4a76223e9921609ec66ce3
Abdelrahman Ahmed <97863124+AbdelrahmanAhmed81@users.noreply.github.com>
2024-09-24 07:10:25 +0300

    LeetCode - Palindrome Number

--- Problems/Solutions.cpp
+++ Problems/Solutions.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+
 using namespace std;
 
 class Solutions
 {
 public:
-    // https://leetcode.com/problems/roman-to-integer/description/
-    static int RomanToInteger(string romanNumber)
+    // 1) https://leetcode.com/problems/roman-to-integer/description/
+    /*static int RomanToInteger(string romanNumber)
     {
         int total = 0;
         for (int i = 0; i < romanNumber.length(); i++)
@@ -63,6 +66,23 @@ public:
         }
 
         return total;
-    }
+    }*/
+    //----------------------------------------------------------
+    // 2) https://leetcode.com/problems/palindrome-number/description/
+    /*static bool isPalindrome(int number)
+    {
+        if (number < 0)
+            return false;
+
+        int tmp = number;
+        long revert = 0;
+        do
+        {
+            revert *= 10;
+            revert += (tmp % 10);
+            tmp /= 10;
+        } while (tmp > 0);
+        return (revert == number);
+    }*/
     //----------------------------------------------------------
 };
--- Problems/main.cpp
+++ Problems/main.cpp
@@ -5,10 +5,13 @@ using namespace std;
 
 int main()
 {
-    // https://leetcode.com/problems/roman-to-integer/description/
-    int result = Solutions::RomanToInteger("XXVII");
-    cout << result << endl;
+    // 1) https://leetcode.com/problems/roman-to-integer/description/
+    /*int result = Solutions::RomanToInteger("XXVII");
+    cout << result << endl;*/
     //----------------------------------------------------------
-
+    // 2) https://leetcode.com/problems/palindrome-number/description/
+    /*bool result = Solutions::isPalindrome(9);
+    cout << (result ? "true" : "false") << endl;*/
+    //----------------------------------------------------------
     return 0;
 }
